702e6739f2a2c29c86f7171c3561aa82ec68a5a2
Kittipat13 <kittipat.li@ku.th>
2025-09-04 18:00:19 +0700

    Add files via upload

---
+++ lab10/FactoryMethodPattern/ShipmentFactory.java
@@ -0,0 +1,16 @@
+package FactoryMethodPattern;
+
+/**
+ * Factory สำหรับสร้างออบเจก Shipment
+ */
+
+public class ShipmentFactory {
+    public Shipment createShipment(String type){
+        if("STANDARD".equalsIgnoreCase(type)){
+            return new StandardShipment();
+        }else if ("EXPRESS".equalsIgnoreCase(type)){
+            return new ExpressShipment();
+        }
+        throw new IllegalArgumentException("Unknow shipment type : " + type);
+    }
+}
---
+++ lab10/FactoryMethodPattern/StandardShipment.java
@@ -0,0 +1,19 @@
+package FactoryMethodPattern;
+
+/**
+ * Concrete Product การจัดส่งแบบมารฐาน
+ */
+
+public class StandardShipment implements Shipment {
+
+    @Override
+    public String getInfo() {
+        return "Standard Delivery";
+    }
+
+    @Override
+    public double getCost() {
+        return 50.0 ;
+    }
+    
+}
